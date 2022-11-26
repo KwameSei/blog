0accabfa278c1adc0f6e5ba12304785ddc2b5f90
KwameSei <nathanielosei92@gmail.com>
2022-11-26 12:38:52 +0000

    Python static data from backend created

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from data import Articles
 
 #Creating instance of flask
 app = Flask(__name__)
+
+get_articles = Articles()
 
 @app.route('/')
 def home():
@@ -10,6 +13,14 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = get_articles) #Extracting data articles
+
+@app.route('/single_article/<string:id>/')
+def articles(id):
+    return render_template('single_article.html', id=id)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, hic temporibus explicabo illum veritatis fuga nemo magnam aut laborum mollitia suscipit cum reiciendis perferendis nulla beatae enim quibusdam facere ipsum.',
+            'author': 'Nathaniel Osei',
+            'created_at': '26-11-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, hic temporibus explicabo illum veritatis fuga nemo magnam aut laborum mollitia suscipit cum reiciendis perferendis nulla beatae enim quibusdam facere ipsum.',
+            'author': 'Nat Osei',
+            'created_at': '26-11-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, hic temporibus explicabo illum veritatis fuga nemo magnam aut laborum mollitia suscipit cum reiciendis perferendis nulla beatae enim quibusdam facere ipsum.',
+            'author': 'Osei Kwame',
+            'created_at': '26-11-2020'
+        },
+    ]
+    #Return the data to be able to use it
+    return articles
